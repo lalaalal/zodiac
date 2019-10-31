511620846d86922b7d83f31c8cd251911b8d4383
lalaalal <100madalala@gmail.com>
2019-10-31 12:54:15 +0900

    .

---
+++ src/main/java/org/zodiac/delivery/model/Status.java
@@ -0,0 +1,8 @@
+package org.zodiac.delivery.model;
+
+public class Status {
+
+    public static final Boolean STATUS_SUCCEED = true;
+    public static final Boolean STATUS_FAILED = false;
+    
+}
